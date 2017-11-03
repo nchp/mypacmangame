fb10e51db67ce040f9682a3274c4378693d90cb8
Nicha Limmanee <nicha.lim@ku.th>
2017-11-03 15:23:39 +0700

    Moving pacman.

--- core/src/com/pacman/game/GameScreen.java
+++ core/src/com/pacman/game/GameScreen.java
@@ -1,6 +1,8 @@
 package com.pacman.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -8,16 +10,24 @@ public class GameScreen extends ScreenAdapter {
     private PacmanGame pacmanGame;
     private Texture pacmanImg;
     
+    private int x;
+    private int y;
+    
     public GameScreen(PacmanGame pacmanGame) {
         this.pacmanGame = pacmanGame;
         pacmanImg = new Texture("pacman.png");
+        x = 100;
+        y = 100;
     }
     
     @Override
     public void render(float delta) {
+    	Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    	x += 5;
     	SpriteBatch batch = pacmanGame.batch;
         batch.begin();
-        batch.draw(pacmanImg, 100, 100);
+        batch.draw(pacmanImg, x, y);
         batch.end();
     }
 }
