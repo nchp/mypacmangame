4f6ec7ed90d2e3d46eb5c099b3e8e39dfaf81e3f
Nicha Limmanee <nicha.lim@ku.th>
2017-11-03 15:56:49 +0700

    Modified move() in Pacman and update() in GameScrren.

--- core/src/com/pacman/game/GameScreen.java
+++ core/src/com/pacman/game/GameScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class GameScreen extends ScreenAdapter {
     private PacmanGame pacmanGame;
@@ -30,22 +31,23 @@ public class GameScreen extends ScreenAdapter {
             	
     	SpriteBatch batch = pacmanGame.batch;
         batch.begin();
-        batch.draw(pacmanImg, x, y);
+        Vector2 pos = pacman.getPosition();
+        batch.draw(pacmanImg, pos.x, pos.y);
         batch.end();
     }
     
     private void update(float delta) {
         if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-            x -= 10;
+        	pacman.move(Pacman.DIRECTION_LEFT);
         }
         if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-            x += 10;
+        	pacman.move(Pacman.DIRECTION_RIGHT);
         }
         if(Gdx.input.isKeyPressed(Keys.UP)) {
-            y += 10;
+        	pacman.move(Pacman.DIRECTION_UP);
         }
         if(Gdx.input.isKeyPressed(Keys.DOWN)) {
-            y -= 10;
+        	pacman.move(Pacman.DIRECTION_DOWN);
         }
     }
 }
