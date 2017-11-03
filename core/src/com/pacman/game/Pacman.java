1ff21a24d2b62d05a32659484bd9bb3a298e056d
Nicha Limmanee <nicha.lim@ku.th>
2017-11-03 15:58:09 +0700

    Create SPEED = 10.

--- core/src/com/pacman/game/Pacman.java
+++ core/src/com/pacman/game/Pacman.java
@@ -18,6 +18,8 @@ public class Pacman {
     public static final int DIRECTION_LEFT = 4;
     public static final int DIRECTION_STILL = 0;
     
+    public static final int SPEED = 10;
+    
     public Pacman(int x, int y) {
         position = new Vector2(x,y);
     }    
@@ -27,7 +29,7 @@ public class Pacman {
     }
     
     public void move(int dir) { 
-    	position.x += 10 * DIR_OFFSETS[dir][0];
-        position.y += 10 * DIR_OFFSETS[dir][1];
+    	position.x += SPEED * DIR_OFFSETS[dir][0];
+        position.y += SPEED * DIR_OFFSETS[dir][1];
     }
 }
