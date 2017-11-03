21252757d1ccb3dfde78f28811abbc9324a735a0
Nicha Limmanee <nicha.lim@ku.th>
2017-11-03 17:05:04 +0700

    Create WolrdRenderer and apply it with class GameScreen.

--- core/src/com/pacman/game/GameScreen.java
+++ core/src/com/pacman/game/GameScreen.java
@@ -5,18 +5,17 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 public class GameScreen extends ScreenAdapter {
     private PacmanGame pacmanGame;
-    
     private Texture pacmanImg;
     private Pacman pacman;
     // private int x;
     // private int y;
     
     World world;
+    WorldRenderer worldRenderer;
     
     public GameScreen(PacmanGame pacmanGame) {
         this.pacmanGame = pacmanGame;
@@ -24,21 +23,18 @@ public class GameScreen extends ScreenAdapter {
 	    // pacman = new Pacman(100,100);
         
         world = new World(pacmanGame);
+        worldRenderer = new WorldRenderer(pacmanGame, world);
         pacman = world.getPacman();
     }
     
     @Override
     public void render(float delta) {
-    	update(delta);
-    	
-    	Gdx.gl.glClearColor(0, 0, 0, 1);
+        update(delta);
+        
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-            	
-    	SpriteBatch batch = pacmanGame.batch;
-        batch.begin();
-        Vector2 pos = pacman.getPosition();
-        batch.draw(pacmanImg, pos.x, pos.y);
-        batch.end();
+ 
+        worldRenderer.render(delta);
     }
     
     private void update(float delta) {
---
+++ core/src/com/pacman/game/WorldRenderer.java
@@ -0,0 +1,26 @@
+package com.pacman.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class WorldRenderer {
+	private PacmanGame pacmanGame;
+	private World world;
+	private SpriteBatch batch;
+	private Texture pacmanImg;
+	
+    public WorldRenderer(PacmanGame pacmanGame, World world) {
+        this.pacmanGame = pacmanGame;
+        batch = pacmanGame.batch;
+        this.world = world;
+        pacmanImg = new Texture("pacman.png");
+    }
+    
+    public void render(float delta) {
+        batch.begin();
+        Vector2 pos = world.getPacman().getPosition();
+        batch.draw(pacmanImg, pos.x, pos.y);
+        batch.end();
+    }
+}
